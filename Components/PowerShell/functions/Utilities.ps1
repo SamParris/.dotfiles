d63f40ac8b5a04602137ba491e2f5ec0fc5d678f
Sam Parris <sam.parris@outlook.com>
2026-05-29 19:35:31 +0100

    Add PowerShell profile loader and initial utility functions

---
+++ Components/PowerShell/functions/Utilities.ps1
@@ -0,0 +1,29 @@
+function Open-Dotfiles {
+<#
+.SYNOPSIS
+    Edit the main dotfiles folder.
+.DESCRIPTION
+    Simple function to open VSCode to edit the main dotfiles folder.
+#>
+    [CmdletBinding()]
+    [Alias("dot")]
+    Param(
+    )
+
+    code "$Home\.dotfiles"
+}
+
+function Update-Profile {
+<#
+.SYNOPSIS
+    Reloads my current PowerShell profile.
+.DESCRIPTION
+    Reloads my current PowerShell profile into the running session.
+#>
+    [CmdletBinding()]
+    [Alias("reload")]
+    Param(
+    )
+
+    . $PROFILE
+}
