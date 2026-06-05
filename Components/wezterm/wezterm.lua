3b759a7b454b27ce7cb42fb7b94900bf569c5deb
Sam Parris <sam.parris@outlook.com>
2026-06-05 17:01:50 +0100

    Added filesystem helpers and updated WezTerm.lua

--- Components/wezterm/wezterm.lua
+++ Components/wezterm/wezterm.lua
@@ -15,6 +15,8 @@ config.window_decorations = "TITLE|RESIZE"
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = true
 config.enable_scroll_bar = false
+config.initial_rows = 30
+config.initial_cols = 125
 
 config.window_padding = {
     left = 10,
