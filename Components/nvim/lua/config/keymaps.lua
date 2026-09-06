91ad039ae9a2ceba1ebf3e692d860225d8cec87b
Sam <sam.parris@outlook.com>
2026-09-06 19:33:15 +0100

    Added Telescope file searching

--- Components/nvim/lua/config/keymaps.lua
+++ Components/nvim/lua/config/keymaps.lua
@@ -7,3 +7,20 @@ map("n", "<leader>w", "<cmd>write<cr>", {
 map("n", "<leader>q", "<cmd>quit<cr>", {
     desc = "Quit"
 })
+
+-- Telescope
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {
+    desc = "Find files"
+})
+
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {
+    desc = "Find text"
+})
+
+map("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {
+    desc = "Find buffers"
+})
+
+map("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {
+    desc = "Find help"
+})
