784c8596bab831205480a4d7b0ad79f3bc0aef8b
Sam Parris <57626214+SamParris@users.noreply.github.com>
2026-09-13 20:45:01 +0100

    Adding neovim configuration (#48)
    * Adding base neovim configuration
    
    * Added neovim keymaps
    
    * Added lazy.nvim and tokyonight scheme
    
    * Added Telescope file searching
    
    * Added which-key
    
    * Added lualine
    
    * Added Treesitter and syntax highlighting

---
+++ Components/nvim/lua/config/options.lua
@@ -0,0 +1,32 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+
+opt.termguicolors = true
+opt.cursorline = ture
+opt.signcolumn = "yes"
+opt.wrap = false
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.swapfile = true
+opt.backup = false
+opt.undofile = true
+
+opt.clipboard = "unnamedplus"
+
+opt.timeoutlen = 500
+opt.updatetime = 250
